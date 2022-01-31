0f6fe7fa3fc9b2b400d32e5675a7a15af3503bec
lutfiqaraman <lutfiqaraman@gmail.com>
2022-01-31 22:01:05 +0200

    create a custom action

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,15 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return new Response(content: '<h1>Main Page</h1>');
+    }
+
+    #[Route('/custom/{name?}', name: 'custom')]
+    public function custom(Request $request) : Response
+    {
+        $name = ($request)->get('name');
+        $content = '<p>Hello ' . $name .' ... </p>';
+
+        return new Response(content: $content);
     }
 
 
